e975760262a6564d88d4329d33032b69c883c371
whereareiam <mmm.g032@gmail.com>
2025-04-21 17:36:27 +0200

    feat(commands): enhance ExampleCommand with styled embed replies

--- yueplugin-common/src/main/java/me/whereareiam/myplugin/common/command/ExampleCommand.java
+++ yueplugin-common/src/main/java/me/whereareiam/myplugin/common/command/ExampleCommand.java
@@ -1,7 +1,9 @@
 package me.whereareiam.myplugin.common.command;
 
+import me.whereareiam.yue.api.StyleKit;
 import me.whereareiam.yue.api.annotation.Command;
 import me.whereareiam.yue.api.output.CommandBase;
+import me.whereareiam.yue.api.util.Translatable;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +11,10 @@ import org.springframework.stereotype.Component;
 public class ExampleCommand implements CommandBase {
 	@Command(name = "example")
 	public void onCommand(SlashCommandInteractionEvent event) {
-		event.reply("Hello World!").setEphemeral(true).queue();
+		event.replyEmbeds(StyleKit.embeds().primary()
+						.setTitle(Translatable.of("plugin.yueplugin.simpleMessage", event.getUser().getIdLong()))
+						.build()
+				).setEphemeral(true)
+				.queue();
 	}
 }
